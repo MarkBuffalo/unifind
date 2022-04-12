96de8b08edc3248b47956a7b593e88357c5a657b
Phil Guimond <phil@grulo.com>
2022-04-11 22:58:53 -0500

    major upgrade

--- unifind.py
+++ unifind.py
@@ -2,13 +2,12 @@
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 from playsound import playsound
+from apscheduler.schedulers.blocking import BlockingScheduler
 import webbrowser
-import requests
 import json
-import sys
-import threading
 
 
 class Unifind:
@@ -17,67 +16,60 @@ class Unifind:
         # This prevents getting spammed with endless geckodriver tabs.
         self.options.headless = True
         self.alarm_sound = "product_alarm.wav"
+        self.options_file = "options.json"
         self.product_list_file = "product_list.txt"
         self.product_list = None
-        self.update_interval = 10.0
+        self.update_interval = 15
         self.opened_urls = []
         self.browser = webdriver.Firefox(options=self.options)
-
-
-    # Load the product_list.txt file and assign it to self.product_list.
-    def load_product_urls(self):
-        with open(self.product_list_file, "r") as f:
-            self.product_list = f.read().splitlines()
+        self.schedule = BlockingScheduler()
+        self.schedule.add_job(self.check_product_urls, "interval", seconds=self.update_interval, max_instances=3)
+        self.schedule.start()
+        self.json_blob = None
 
 
     # The main entry point of the application.
     def check_product_urls(self):
         # So this function repeats itself every X seconds.
-        threading.Timer(self.update_interval, self.check_product_urls).start()
+        try:
+            # Reload, in case the user edited the file.
+            self.load_json()
 
-        # Reload, in case the user edited the file.
-        self.load_product_urls()
+        # Kill the schedule if the program is murderlized.
+        except (KeyboardInterrupt, SystemExit):
+            self.schedule.shutdown()
+            self.browser.close()
 
-        for product_url in self.product_list:
-            self.check_page(product_url)
 
     #  This functions checks pages to see if any products are in stock.
-    def check_page(self, url):
-        page = self.browser.get(url)
-        try:
-            soup = BeautifulSoup(page.page_source)
-            # Close the browser so we don't exhaust computer resources.
-            page.close()
+    def check_page(self, **kwargs):
+        url = kwargs.get("url")
+        self.browser.get(url)
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
 
-            if self.is_product_available(soup):
-                print("[!] Product is available. Opening window.")
-                self.open_browser_to_site(url)
-        except AttributeError:
-            pass
+        # Close the browser so we don't exhaust computer resources.
+
+        if self.is_product_available(soup, **kwargs):
+            self.open_browser_to_site(url, soup)
+
+
+    @staticmethod
+    def get_page_title(soup):
+        return soup.findAll("h1", {"class":"comProduct__title"})[0].text
 
     # Boolean function used by self.check_page to see if the product is available.
     @staticmethod
-    def is_product_available(soup):
-        # Single product pages with single packs have a different set of (default) options.
-        if not soup.findAll("span", {"class": "relatedProducts__item__data__price"})[0].text == "Sold Out":
+    def is_product_available(soup, **kwargs):
+        if not soup.findAll(kwargs.get("element"), {kwargs.get("key"): kwargs.get("value")})[0].text == kwargs.get("query"):
             return True
-
-        # The camera page has a different set of options.
-        if not soup.findAll("span", {"class": "comProduct__badge"})[0].text == "Sold Out":
-            return True
-
-        # Third product page, such as video receivers.
-        if soup.findAll("button", {"class": "btn btn--full"})[0].text == "Sold Out":
-            return True
-
         return False
 
-
     # This function opens a browser if available.
-    def open_browser_to_site(self, url):
-        self.play_alarm()
+    def open_browser_to_site(self, url, soup):
         if url not in self.opened_urls:
-            webbrowser.get('firefox').open_new_tab(url)
+            print(f"[!] Product {self.get_page_title(soup)} is available @ {url}. Opening window.")
+            self.play_alarm()
+            webbrowser.get("firefox").open_new_tab(url)
             self.opened_urls.append(url)
 
     # This function plays a sound before opening the browser.
@@ -85,6 +77,35 @@ class Unifind:
         playsound(self.alarm_sound)
 
 
+    # Load json from the options file.
+    def load_json(self):
+        with open(self.options_file, "r") as f:
+            self.json_blob = json.loads(f.read())
+
+        for item in self.json_blob:
+            self.parse_json_element(item)
+
+
+    # Get the json response from each item.
+    def parse_json_element(self, item):
+        url = item.get("url")
+        html_element = item.get("element")
+        key = item.get("key")
+        value = item.get("value")
+        query = item.get("query")
+        self.check_page(url=url, element=html_element, key=key, value=value, query=query)
+
+
 if __name__ == "__main__":
-    u = Unifind()
-    u.check_product_urls()
+    try:
+        u = Unifind()
+    except ConnectionRefusedError:
+        pass
+    except ConnectionAbortedError:
+        pass
+    except ConnectionResetError:
+        pass
+    except TimeoutError:
+        pass
+    except TimeoutException:
+        pass
